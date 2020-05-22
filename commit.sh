a989e8f9387ddeb6d3d0c6804fec0d5b39d6e01a
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-22 14:41:14 -0500

    removed layers from docker images

--- commit.sh
+++ commit.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 set -exu
+
+if [ $# -eq 0 ] ; then
+M='auto update'
+else
+M="$*"
+fi
+
 cd `dirname $(readlink -f $0)`/..
+
 for k in */ ; do (
    set +e
    cd $k
    [ -d .git ] || continue
    git add .
-   git commit -m 'auto update'
+   git commit -m "$M"
    git push origin master
 ) & sleep 1 ; done
