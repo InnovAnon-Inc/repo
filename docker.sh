72c864c074f3b1e55f52282f47413801437a0ecf
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-24 00:08:12 -0500

    auto update

---
+++ docker.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+set -exu
+
+cd "`dirname "$(readlink -f "$0")"`"/..
+
+for k in poobuntu{,-{dev,ci}} docker-apt-cacher-ng Abaddon docker-{wads,wine,zandronum,doom} ; do (
+  set -exu
+  cd $k
+  if [ -f runs.sh ] ; then ./runs.sh
+  else                     ./run.sh
+  fi
+) || { echo $k; break; } ; done
+
