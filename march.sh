781cace8430589ea4b14d6b696e75adce018bf7a
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-24 22:37:41 -0500

    bugfix

--- march.sh
+++ march.sh
@@ -5,6 +5,6 @@ set -eu
 #TUNE=`gcc -march=$ARCH  -Q --help=target | grep -- '-mtune=' | cut -f3`
 ARCH=`gcc -march=native -Q --help=target | awk '$1 == "-march=" {print $2}'`
 TUNE=`gcc -march=$ARCH  -Q --help=target | awk '$1 == "-mtune=" {print $2}'`
-echo ARCH=$ARCH
-echo TUNE=$TUNE
+echo $ARCH
+echo $TUNE
 
