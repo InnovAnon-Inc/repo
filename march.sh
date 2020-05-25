297524013a134b05dc6bc5e68141f2367e9f07e0
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-24 22:26:38 -0500

    made march output eval-friendly

--- march.sh
+++ march.sh
@@ -2,5 +2,7 @@
 set -exu
 
 ARCH=`gcc -march=native -Q --help=target | grep -- '-march=' | cut -f3`
-gcc -march=$ARCH  -Q --help=target | grep -- '-mtune=' | cut -f3
+TUNE=`gcc -march=$ARCH  -Q --help=target | grep -- '-mtune=' | cut -f3`
+echo ARCH=$ARCH
+echo TUNE=$TUNE
 
