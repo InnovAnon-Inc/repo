ef1e6ae94e43ba8138c9ca8e36439f57b98c57b8
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-24 20:27:23 -0500

    auto update by ./commit.sh

--- commit.sh
+++ commit.sh
@@ -9,6 +9,7 @@ fi
 
 cd "`dirname "$(readlink -f "$0")"`"/..
 
+declare -a pids
 for k in */ ; do (
    set +e
    cd $k
@@ -17,5 +18,26 @@ for k in */ ; do (
    git commit -m "$M"
    #git push origin master
    git push origin
-) & sleep 1 ; done
+) & pids+=($?) ; sleep 1 ; done
+
+waitall() { # PID...
+  ## Wait for children to exit and indicate whether all exited with 0 status.
+  local errors=0
+  while : ; do
+    for pid in "$@"; do
+      shift
+      if kill -0 "$pid" 2>/dev/null; then
+        set -- "$@" "$pid"
+      elif ! wait "$pid"; then
+        ((++errors))
+      fi
+    done
+    (("$#" > 0)) || break
+    # TODO: how to interrupt this sleep when a child terminates?
+    sleep ${WAITALL_DELAY:-1}
+   done
+  ((errors == 0))
+}
+
+waitall ${pids[@]}
 
