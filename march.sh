004d979c5c54f0fdc871c0ab2e6029ebb74307df
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-24 22:33:23 -0500

    suppressed debugging output

--- march.sh
+++ march.sh
@@ -3,8 +3,8 @@ set -eu
 
 #ARCH=`gcc -march=native -Q --help=target | grep -- '-march=' | cut -f3`
 #TUNE=`gcc -march=$ARCH  -Q --help=target | grep -- '-mtune=' | cut -f3`
-ARCH=`gcc -march=native -Q --help=target | awk '$1 == "-march=" {print $2}'
-TUNE=`gcc -march=$ARCH  -Q --help=target | awk '$1 == "-mtune=" {print $2}'
+ARCH=`gcc -march=native -Q --help=target | awk '$1 == "-march=" {print $2}'`
+TUNE=`gcc -march=$ARCH  -Q --help=target | awk '$1 == "-mtune=" {print $2}'`
 echo ARCH=$ARCH
 echo TUNE=$TUNE
 
