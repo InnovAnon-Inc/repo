3c16a1d781fdba9256053b9c4a3fa83f297df7bf
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-27 14:02:43 -0500

    auto update by /home/momobiblebender/src/repo/commit-sequential.sh

--- readme-c.sh
+++ readme-c.sh
@@ -19,19 +19,22 @@ for PROJECT in */ ; do
 
   #cat << EOF
   cat >> $PROJECT/README.md << EOF
+[![CircleCI](https://img.shields.io/circleci/build/github/InnovAnon-Inc/${PROJECT}?color=%23FF1100&logo=InnovAnon%2C%20Inc.&logoColor=%23FF1133&style=plastic)](https://circleci.com/gh/InnovAnon-Inc/${PROJECT})
+
+[![Latest Release](https://img.shields.io/github/commits-since/InnovAnon-Inc/${PROJECT}/latest?color=%23FF1100&include_prereleases&logo=InnovAnon%2C%20Inc.&logoColor=%23FF1133&style=plastic)](https://github.com/InnovAnon-Inc/${PROJECT}/releases/latest)
+
+[![Repo Size](https://img.shields.io/github/repo-size/InnovAnon-Inc/${PROJECT}?color=%23FF1100&logo=InnovAnon%2C%20Inc.&logoColor=%23FF1133&style=plastic)](https://github.com/InnovAnon-Inc/${PROJECT})
+
+![Dependencies](https://img.shields.io/librariesio/github/InnovAnon-Inc/${PROJECT}?color=%23FF1100&style=plastic)
+
+[![License Summary](https://img.shields.io/github/license/InnovAnon-Inc/${PROJECT}?color=%23FF1100&label=Free%20Code%20for%20a%20Free%20World%21&logo=InnovAnon%2C%20Inc.&logoColor=%23FF1133&style=plastic)](https://tldrlegal.com/license/unlicense#summary)
+
 ![Corporate Logo](https://i.imgur.com/UD8y4Is.gif)
 
+[![CodeFactor](https://www.codefactor.io/repository/github/InnovAnon-Inc/${PROJECT}/badge)](https://www.codefactor.io/repository/github/InnovAnon-Inc/${PROJECT})
+
+[![LoC](https://tokei.rs/b1/github/InnovAnon-Inc/${PROJECT}?category=code)](https://github.com/InnovAnon-Inc/${PROJECT})
+
 EOF
-#[![CircleCI](https://img.shields.io/circleci/build/github/InnovAnon-Inc/${PROJECT}?color=%23FF1100&logo=InnovAnon%2C%20Inc.&logoColor=%23FF1133&style=plastic)](https://circleci.com/gh/InnovAnon-Inc/${PROJECT})
-#
-#[![Latest Release](https://img.shields.io/github/commits-since/InnovAnon-Inc/${PROJECT}/latest?color=%23FF1100&include_prereleases&logo=InnovAnon%2C%20Inc.&logoColor=%23FF1133&style=plastic)](https://github.com/InnovAnon-Inc/${PROJECT}/releases/latest)
-#
-#[![Repo Size](https://img.shields.io/github/repo-size/InnovAnon-Inc/${PROJECT}?color=%23FF1100&logo=InnovAnon%2C%20Inc.&logoColor=%23FF1133&style=plastic)](https://github.com/InnovAnon-Inc/${PROJECT})
-#
-#![Dependencies](https://img.shields.io/librariesio/github/InnovAnon-Inc/${PROJECT}?color=%23FF1100&style=plastic)
-#
-#[![License Summary](https://img.shields.io/github/license/InnovAnon-Inc/${PROJECT}?color=%23FF1100&label=Free%20Code%20for%20a%20Free%20World%21&logo=InnovAnon%2C%20Inc.&logoColor=%23FF1133&style=plastic)](https://tldrlegal.com/license/unlicense#summary)
-#
-#EOF
 done
 
