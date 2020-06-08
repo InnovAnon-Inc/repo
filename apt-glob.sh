01bab595aff5d3783755d7ff712f9385410459d5
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-06-08 05:31:28 +0000

    bugfix

--- apt-glob.sh
+++ apt-glob.sh
@@ -1,12 +1,29 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-{ for k in $* ; do
+NO_GLOB="${NO_GLOB:-0}"
+while getopts "g" arg; do
+  case $arg in
+    g)
+      NO_GLOB=1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+
+if (( $# )) ; then
+  for k in $* ; do
     echo "${k##^#.*}"
-  done        ;
-  sed '/^#/d' ;
-  cat         ; } |
-xargs -I @ sh -c  \
-  "apt list '@' 2> /dev/null | \
-   awk -F / 'NR > 1 {print \$1}'"
+  done
+else
+  sed '/^#/d'
+fi |
+if (( "$NO_GLOB" )) ; then
+  cat
+else
+  xargs -I @ sh -c  \
+    "apt list '@' 2> /dev/null | \
+     awk -F / 'NR > 1 {print \$1}'"
+fi
 
