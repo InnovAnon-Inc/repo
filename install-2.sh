824f90d856c84693c0d6a8780c3d5c5f1ae33c7a
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-06-10 00:15:23 -0500

    install v2

---
+++ install-2.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+set -euxo pipefail
+
+. `command -v delete.env`
+(( $# ))
+
+REPO=https://raw.githubusercontent.com/InnovAnon-Inc/repo/master
+
+sed -e '/^#/d' \
+    -e '/^$/d' \
+    $@         |
+{ cd        /usr/local/bin ;
+  while read line ; do
+    l="${line%.sh}"
+    if (( "$D" )) ; then
+      rm   -fv    "$l"                  || exit $?
+    else
+      curl  -L -o "$line" "$REPO/$line" || exit $?
+      chmod -v +x "$line"               || exit $?
+      mv    -v    "$line" "$l"          || exit $?
+    fi
+  done ; 
+}
+
