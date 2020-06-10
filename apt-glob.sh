52b5e0c00975b73c732619e1d1e8f01a44eb78af
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-06-10 01:54:21 -0500

    replaced file

--- apt-glob.sh
+++ apt-glob.sh
@@ -1,1 +1,26 @@
-404: Not Found
+#! /usr/bin/env bash
+set -euo pipefail
+
+NO_GLOB="${NO_GLOB:-0}"
+ECHO="${ECHO:-}"
+while getopts "ge" arg; do
+  case $arg in
+    g)
+      NO_GLOB=1
+      ;;
+    e)
+      ECHO=-e
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+apt-list $ECHO $* |
+if (( "$NO_GLOB" )) ; then
+  cat
+else
+  xargs -I @ $SHELL -c  \
+    "apt list '@' 2> /dev/null | \
+     awk -F / 'NR > 1 {print \$1}'"
+fi
+
